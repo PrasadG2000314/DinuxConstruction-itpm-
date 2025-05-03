b4eb7240316881d0221fb4b1c0b521d0aae149a1
Nipun Prasad Gunawardhana <nipungunawardhana71@gmail.com>
2025-05-03 12:45:08 +0530

    server fix

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import logger from "./utils/logger.js";
 import connectDatabase from "./utils/database.js";
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     logger.info(`Server has started and running on PORT ${PORT}`);
