b238af2ad165773a89fa8378a54dcb47f746dbac
Janith Gunarathne <127580773+Janith0113@users.noreply.github.com>
2025-05-03 13:25:54 +0530

    add updates

--- server/routes/StockRouter.js
+++ server/routes/StockRouter.js
@@ -1,10 +1,17 @@
 import express from 'express';
-const router = express.Router();
+import StockController from '../controller/StockController.js';
+import authorizeUser from '../middleware/authorizeUser.js';
 
-// Define your routes here
-router.get('/', (req, res) => {
-  res.send('Stock route');
-});
+const stockRouter = express.Router();
 
-// Export the router as default
-export default router;
+stockRouter.get('/getid', authorizeUser, StockController.generateStockId);
+stockRouter.post('/create', authorizeUser, StockController.createStock);
+stockRouter.get('/get/:equipmentId', authorizeUser, StockController.getStockById);
+stockRouter.get('/getall', authorizeUser, StockController.getAllStock);
+stockRouter.put('/update', authorizeUser, StockController.updateStock);
+stockRouter.delete('/delete/:equipmentId', authorizeUser, StockController.deleteStock);
+
+stockRouter.post('/buy', authorizeUser, StockController.buyStock);
+stockRouter.get('/getbought/:month', authorizeUser, StockController.getBoughtStockDetailsByMonth);
+
+export default stockRouter;
